18f56b70c2c57774625c6d98d601222f7ba22ec3
xjw <xujiaweireal@gmail.com>
2017-01-03 19:12:50 +0800

    add photoView

--- NightReader/app/src/main/java/com/example/pc/nightreader/ui/adapter/PhotoPagerAdapter.java
+++ NightReader/app/src/main/java/com/example/pc/nightreader/ui/adapter/PhotoPagerAdapter.java
@@ -1,8 +1,8 @@
 package com.example.pc.nightreader.ui.adapter;
 
-import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,36 +12,39 @@ import com.example.pc.nightreader.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
+
 /**
  * Created by xujiawei on 2016/12/12.
  */
 
 public class PhotoPagerAdapter extends PagerAdapter  {
    private   List<String> mUrlList=new ArrayList<>();
-   private  Context mContext;
+    private  AppCompatActivity mActivity;
 
-    public PhotoPagerAdapter(List<String > pUrlList, Context pContext) {
+    public PhotoPagerAdapter(List<String > pUrlList, AppCompatActivity  pActivity) {
         this.mUrlList = pUrlList;
-        this.mContext = pContext;
+        this.mActivity = pActivity;
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        View view = (View) LayoutInflater.from(mContext).inflate(R.layout.img_browse, null);
-        //PhotoView img=(PhotoView)view.findViewById(R.id.photoview);
-      //图片加载
+        View view = (View) LayoutInflater.from(mActivity).inflate(R.layout.img_browse, null);
+        PhotoView img=(PhotoView)view.findViewById(R.id.photoview);
+        //图片加载
         ((ViewPager) container).addView(view);
-       /* img.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
+        img.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
             @Override
             public void onPhotoTap(View view, float x, float y) {
-                Log.e("setOnViewTapListener","xujiawei");
+
             }
 
             @Override
             public void onOutsidePhotoTap() {
-                mcontext.finish();
+             mActivity.finish();
             }
-        });*/
+        });
         return view;
     }
 
